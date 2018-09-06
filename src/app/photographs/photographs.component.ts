4ca4efcf256a400c715d1fe19007d9c4e5d01342
unknown <valentin.hanard@laposte.net>
2018-09-06 16:56:26 +0200

    v1 photographs design

--- src/app/photographs/photographs.component.ts
+++ src/app/photographs/photographs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-photographs',
@@ -9,9 +10,16 @@ import { Router } from '@angular/router';
 export class PhotographsComponent implements OnInit {
 
   private urlKartina: string;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+  
+  private tabPhoto: Array<number> = new Array<number>();
 
   constructor(private router : Router) {
     this.urlKartina = this.router.url;
+    for (let index = 0; index < 24; index++) {
+      this.tabPhoto.push(index);
+    }
   }
 
   ngOnInit() {
